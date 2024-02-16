9d1bb22557e02bf2ea67a5d0683bb8887997f06f
Kacper Grudziński <kgrvdzin@gmail.com>
2024-02-16 18:27:40 +0100

    added js--confetti

--- src/main.js
+++ src/main.js
@@ -1,20 +1,32 @@
+import JSConfetti from "js-confetti";
+const jsConfetti = new JSConfetti();
 const addButton = document.querySelector(".add--js");
 const subtractButton = document.querySelector(".subtract--js");
 const counterValue = document.querySelector(".counter--js");
+const hour = new Date().getHours();
+const actualMinute = new Date().getMinutes();
 
 let glasses = 0;
 
-const key = new Date().toISOString().slice(0, 10);
+let key = new Date().toLocaleString().slice(0, 10);
 console.log(key);
 
+setInterval(() => {
+  console.log("tik-tak");
+  if (key !== new Date().toLocaleString().slice(0, 10)) {
+    key = new Date().toLocaleString().slice(0, 10);
+    setCounterValue(0);
+    glasses = 0;
+  }
+}, 1000 * 60);
 
-if(localStorage.getItem(key)) {
-    glasses = parseInt(localStorage.getItem(key));
+if (localStorage.getItem(key)) {
+  glasses = parseInt(localStorage.getItem(key));
 }
 
 const setCounterValue = (value) => {
   counterValue.innerHTML = value;
-  localStorage.setItem(key, value)
+  localStorage.setItem(key, value);
 };
 
 setCounterValue(glasses);
@@ -30,3 +42,9 @@ subtractButton.addEventListener("click", () => {
     setCounterValue(glasses);
   }
 });
+
+setInterval(() => {
+  if(hour === actualMinute){
+    jsConfetti.addConfetti();
+  }
+}, 1000*60);
